2721c2777d1d5d20cadd7ef61c6fdd8d1f7658be
Jackson Ming Hu <huming2207@gmail.com>
2020-06-21 14:52:37 +1000

    Take out dialog box

---
+++ src/components/InfoDialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-dialog v-model="show" width="500">
+    <v-card>
+      <v-card-title class="headline">
+        {{ dialogTitle }}
+      </v-card-title>
+
+      <v-card-text>
+        {{ dialogText }}
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click.stop="show = false">
+          Okay
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class InfoDialog extends Vue {
+  @Prop(String) dialogTitle = "";
+  @Prop(String) dialogText = "";
+
+  // Ref: https://stackoverflow.com/a/49119008
+  get show(): boolean {
+    return this.$attrs.value as any; // Workaround for wrong typings
+  }
+
+  set show(val: boolean) {
+    this.$emit("input", val);
+  }
+}
+</script>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -37,24 +37,7 @@
           </v-card>
         </v-col>
       </v-row>
-      <v-dialog v-model="dialog" width="500">
-        <v-card>
-          <v-card-title class="headline">
-            {{ dialogTitle }}
-          </v-card-title>
-
-          <v-card-text>
-            {{ dialogText }}
-          </v-card-text>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="primary" @click="dialog = !dialog">
-              Okay
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+      <InfoDialog v-model="showDialog" :dialogTitle="dialogTitle" :dialogText="dialogText" />
     </v-container>
   </div>
 </template>
@@ -62,6 +45,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import { validationMixin } from "vuelidate";
 import { required } from "vuelidate/lib/validators";
+import InfoDialog from "./InfoDialog.vue";
 
 @Component({
   mixins: [validationMixin],
@@ -69,13 +53,14 @@ import { required } from "vuelidate/lib/validators";
     username: { required },
     password: { required },
   },
+  components: { InfoDialog },
 })
 export default class LoginForm extends Vue {
   username = "";
   password = "";
-  dialog = false;
   dialogTitle = "";
   dialogText = "";
+  showDialog = false;
 
   async performLogin(): Promise<void> {
     this.$v.$touch();
@@ -84,7 +69,7 @@ export default class LoginForm extends Vue {
       if (!resp.data.data || !resp.data.data.token) {
         this.dialogText = resp.data.message;
         this.dialogTitle = "Something went wrong...";
-        this.dialog = true;
+        this.showDialog = true;
       } else {
         this.$router.push({ path: "/browser" });
       }
@@ -93,11 +78,11 @@ export default class LoginForm extends Vue {
         const resp = err.response;
         this.dialogText = resp.data.message;
         this.dialogTitle = "Something went wrong...";
-        this.dialog = true;
+        this.showDialog = true;
       } else {
         this.dialogText = "Unknown error, please check your Internet connection";
         this.dialogTitle = "Something went wrong...";
-        this.dialog = true;
+        this.showDialog = true;
       }
     }
   }
